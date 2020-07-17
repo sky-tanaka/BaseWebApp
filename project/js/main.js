c879a81c00de806dfe368e0937021520cbba5ef8
sky-tanaka <s-tanaka@orlab.co.jp>
2020-07-17 11:20:40 +0900

    add feature

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,24 @@
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
+// $(document).ready(function(){
+//   getWeather();
+// })
 
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
+function getWeather(searchQuery) {
+  var url ="https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=imperial&appid=395fd614c8f087ab244c1102bef0dc59"
+
+
+  $(".city").text("");
+  $(".temp").text("");
+  $(".error-message").text("");
   
+  $.ajax(url,{success: function(data){
+    $(".city").text(data.name);
+    $(".temp").text(data.main.temp);
+  }, error: function(error){
+    $(".error-message").text("An error occured");
+  }})
+}
+
+function searchWeather() {
+  var searchQuery = $(".search").val();
+  getWeather(searchQuery);
 }
